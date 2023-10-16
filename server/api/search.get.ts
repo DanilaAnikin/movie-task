7af5f1190946cb296fa3a6e206ba18c07cbcf598
DanilaAnikin <danakin10502@gmail.com>
2023-10-16 22:22:56 +0200

    Hidden code

--- server/api/search.get.ts
+++ server/api/search.get.ts
@@ -4,13 +4,16 @@ export default defineEventHandler(async(event)  => {
     const query = getQuery(event);
     const search = query.query;
 
-    const url = new URL('https://api.themoviedb.org/3/search/movie')
+    const myAutoarization = process.env.MY_AUTOARIZATION;
+    const myUrl = process.env.MY_URL;
+
+    const url = new URL(myUrl as string)
     url.searchParams.set('query', search as string)
 
     const response = await fetch(url, {
         headers: {
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOWQ3ZTRlYWQxYmQ3MDk2ZTBmNGI2YzU2ZGE5NTFhOCIsInN1YiI6IjYxNDYxOTgzMmRmZmQ4MDA5MjEzMWVkZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fakHFAMHAkzA4EKmmo2h_nKlDRMFh3bSW91GKmiC0ig'
-            ,accept: 'application/json'
+            Authorization: myAutoarization as string,
+            accept: 'application/json'
         }
     });
 
